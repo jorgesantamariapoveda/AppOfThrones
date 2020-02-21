888f0d0f1f50427217ef589e1ff46c1ed1014c8d
Jorge <jorgesantamariapoveda@gmail.com>
2020-02-21 19:51:22 +0100

    Se inicia RateViewController

---
+++ AppOfThrones/AppOfThrones/RateViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RateViewController.swift
+//  AppOfThrones
+//
+//  Created by Jorge on 21/02/2020.
+//  Copyright © 2020 Jorge. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+    
+}
--- AppOfThrones/AppOfThrones/SceneDelegate.swift
+++ AppOfThrones/AppOfThrones/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow.init(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = RateViewController()
         window?.makeKeyAndVisible()
     }
 
