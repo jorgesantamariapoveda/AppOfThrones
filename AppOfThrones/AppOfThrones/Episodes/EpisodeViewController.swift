7fc304de0aa91062b43a3f51002f9b45eb9cc213
Jorge <jorgesantamariapoveda@gmail.com>
2020-02-21 20:37:20 +0100

    Se añade segmentedControl a EpisodeViewController

--- AppOfThrones/AppOfThrones/Episodes/EpisodeViewController.swift
+++ AppOfThrones/AppOfThrones/Episodes/EpisodeViewController.swift
@@ -9,5 +9,10 @@
 import UIKit
 
 class EpisodeViewController: UIViewController {
+
+    var episodes: [Episode] = [
+        Episode.init(id: 1, name: "Name1", date: "Date1", image: "imagen1", episode: 1, season: 1, overview: "overview1"),
+        Episode.init(id: 2, name: "Name2", date: "Date2", image: "imagen2", episode: 2, season: 2, overview: "overview2")
+    ]
     
 }
---
+++ AppOfThrones/AppOfThrones/Model/Episode.swift
@@ -0,0 +1,30 @@
+//
+//  Episode.swift
+//  AppOfThrones
+//
+//  Created by Jorge on 21/02/2020.
+//  Copyright © 2020 Jorge. All rights reserved.
+//
+
+import Foundation
+
+class Episode {
+
+    var id: Int
+    var name: String?
+    var date: String?
+    var image: String?
+    var episode: Int
+    var season: Int
+    var overview: String
+
+    init(id: Int, name: String?, date: String?, image: String?, episode: Int, season: Int, overview: String) {
+        self.id = id
+        self.name = name
+        self.date = date
+        self.image = image
+        self.episode = episode
+        self.season = season
+        self.overview = overview
+    }
+}
